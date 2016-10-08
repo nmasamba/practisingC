ab06a5c6919fdaab0b60e230642c5dd2fd807250
nmasamba <slimm4rl@yahoo.com>
2016-10-08 13:27:46 +0100

    3d array pointers

---
+++ 21_3darr_ptr.c
@@ -0,0 +1,45 @@
+/*****************************************************************
+
+Author: Nyasha Masamba
+
+This C program simply takes in elements for a 3-dimensional array
+via the console. It then prints the resulting 2x2x2 matrix back to
+the user, in row major order style; however it uses pointers to
+access the data structure's elements.
+
+*****************************************************************/
+
+
+#include <stdio.h>
+
+
+int main() 
+{
+	int i, j, k;
+	int myarray[2][2][2];
+	int (*parr)[2][2] = myarray;
+	
+	printf(" \n Enter the elements of a 2 x 2 x 2 matrix \n");
+	for(i=0; i<2; i++) {
+		for(j=0; j<2; j++) {
+			for(k=0; k<2; k++) {
+				scanf("%d", &myarray[i][j][k]);
+			}
+		}
+	}	
+
+	printf(" \n Your matrix is: ");
+	for(i=0; i<2; i++) {
+		printf("\n");
+		for(j=0; j<2; j++) {
+			printf("\n");
+			for(k=0; k<2; k++) {
+				printf("\t array[%d][%d][%d] = %d", i, j, k, *(*(*(parr+i)+j)+k));	
+			}	
+		}	
+	}
+
+	return 0;
+
+
+}
