608a69f4dbfeae091ce72ceac978ea98eaa17429
nmasamba <slimm4rl@yahoo.com>
2017-12-07 21:08:44 +0000

    bubble sort

--- 75_bubble_sort.c
+++ 75_bubble_sort.c
@@ -11,7 +11,33 @@ elements by repeatedly moving the largest element to the highest index position 
 pairs of elements in the array are compared with each other. If the element at the lower index is 
 greater than the element at the higher index, the two elements are interchanged so that the element 
 is placed before the bigger one. This process will continue till the list of unsorted elements exhausts.
-The complexity of bubble sort algorithm is O(n^2).
+The complexity of bubble sort algorithm is O(n^2). 
+
+If the array is already sorted, we would need to pass over all the elements at least once but we 
+would not need to do a second pass. It would be good to detect when this is the case. We can have 
+a flag that is set to TRUE when swapping is done at any point during the first pass, but it is set
+to FALSE if no swapping is done during the first pass. If the array is already sorted, the algorithm 
+simply exits after the first pass. This is an example of an optimised bubble sort algorithm, 
+implemented as shown below:
+void bubble_sort(int *arr, int n)
+{
+int i, j, temp, flag = 0;
+    for(i=0; i<n; i++)
+ {
+       for(j=0; j<n–i–1; j++)
+ {
+          if(arr[j]>arr[j+1])
+ {
+             flag = 1;
+             temp = arr[j+1];
+             arr[j+1] = arr[j];
+             arr[j] = temp;
+          }
+       }
+      if(flag == 0) // array is sorted
+         return;
+   }
+} 
 
 Example I/O
 
