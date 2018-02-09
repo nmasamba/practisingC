e5fe9d0f31e38545aa83f9c35f5eb62bddaccbff
nmasamba <slimm4rl@yahoo.com>
2018-02-09 18:44:26 +0000

    closed hashing search

--- 84_closed_hashing_search.c
+++ 84_closed_hashing_search.c
@@ -24,33 +24,35 @@ int main()
 {
 	int option;
 
-	for ( i = 0;i < 10;i++ ) //to initialize every element as ‘–1’
-	 ht[i] = –1;
+	for ( i = 0;i < 10;i++ ) //to initialize every element as ‘-1’
+	 	ht[i] = -1;
 	do
 	{
-	printf( "\n MENU \n1.Insert \n2.Search \n3.Delete \n4.Display \n5.Exit");
-	printf( "\n Enter your option.");
-	scanf( "%d", &option);
-	 switch (option)
-	 {
-	 case 1:
-	 insert_val();
-	break;
-	 case 2:
-	 search_val();
-	break;
-	 case 3:
-	 delete_val();
-	break;
-	 case 4:
-	 display();
-	break;
-	   default:
-	printf( "\nInvalid choice entry!!!\n" );
-	 break;
-	 }
-	}while (option!=5);
-	getch();
+		printf( "\n MENU \n1.Insert \n2.Search \n3.Delete \n4.Display \n5.Exit");
+		printf( "\n Enter your option.");
+		scanf( "%d", &option);
+
+		switch (option)
+		{
+			case 1:
+		 		insert_val();
+				break;
+			case 2:
+		 		search_val();
+				break;
+		 	case 3:
+		 		delete_val();
+				break;
+		 	case 4:
+		 		display();
+				break;
+		   default:
+				printf( "\nExiting...\n" );
+		 		break;
+		}
+
+	} while (option!=5);
+
 	return 0;
 }
 
@@ -59,32 +61,35 @@ void insert_val()
 	int val, f = 0;
 	printf( "\nEnter the element to be inserted : " );
 	scanf( "%d", &val );
-	key = ( val % 10 ) – 1;
-	if ( ht[key] == –1 )
-	 {
-	 ht[key] = val;
-	 }
-	 else
-	 {
-	if ( key < 9 )
-	 {
-	   for ( i = key + 1;i < 10;i++ )
-	 {
-	if ( ht[i] == –1 )
-	 {
-	 ht[i] = val;
-	break;
-	 }
-	 }
-	 }
-	for ( i = 0;i < key;i++ )
-	 {
-	   if ( ht[i] == –1 )
-	 {
-	 ht[i] = val;
-	break;
-	 }
-	 }
+	
+	key = ( val % 10 ) - 1;
+	
+	if ( ht[key] == -1 )
+	{
+	 	ht[key] = val;
+	}
+	else
+	{
+		if ( key < 9 )
+	 	{
+			for ( i = key + 1;i < 10;i++ )
+	 		{
+				if ( ht[i] == -1 )
+	 			{
+	 				ht[i] = val;
+					break;
+	 			}
+	 		}
+	 	}
+		
+		for ( i = 0;i < key;i++ )
+	 	{
+			if ( ht[i] == -1 )
+	 		{
+	 			ht[i] = val;
+				break;
+	 		}
+	 	}
 	}
 }
 
@@ -99,42 +104,46 @@ void search_val()
 	int val, flag = 0;
 	printf( "\nEnter the element to be searched :: " );
 	scanf( "%d", &val );
-	key = ( val % 10 ) – 1;
+	
+	key = ( val % 10 ) - 1;
+	
 	if ( ht[ key ] == val )
-	flag = 1;
+		flag = 1;
 	else
 	{
-	for (i = key + 1;i < 10;i++)
-	 {
-	if(ht[i] == val)
-	 {
-	   flag = 1;
-	 key = i;
-	break;
+		for (i = key + 1;i < 10;i++)
+	 	{
+			if(ht[i] == val)
+	 		{
+				flag = 1;
+	 			key = i;
+				break;
+			}
+	 	}
 	}
-	 }
-	}
+
 	if (flag == 0)
 	{
-	for (i = 0;i < key;i++)
-	 {
-	if (ht[ i ] == val)
-	 {
-	   flag = 1;
-	 key = i;
-	break;
-	 }
-	 }
+		for (i = 0;i < key;i++)
+	 	{
+			if (ht[ i ] == val)
+	 		{
+				flag = 1;
+	 			key = i;
+				break;
+	 		}
+	 	}
 	}
+
 	if (flag == 1)
 	{
-	found=1;
-	printf("\n The item searched was found at position %d !", key + 1 );
+		found=1;
+		printf("\n The item searched was found at position %d !", key + 1 );
 	}
 	else
 	{
-	 key = –1;
-	printf( "\nThe item searched was not found in the hash table" );
+		key = -1;
+		printf( "\nThe item searched was not found in the hash table" );
 	}
 }
 
@@ -143,11 +152,11 @@ void delete_val()
 	search_val();
 	if (found==1)
 	{
-	if ( key != –1 )
-	 {
-	printf( "\nThe element deleted is %d ", ht[ key ] );
-	 ht[ key ] = –1;
-	 }
+		if ( key != -1 )
+	 	{
+			printf( "\nThe element deleted is %d ", ht[ key ] );
+	 		ht[ key ] = -1;
+	 	}
 	}
 }
 
